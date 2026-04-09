721d58725e0dc863fd112bb3d6150b565fa0348d
omarsalem22 <omarmohamed25112001@gmail.com>
2026-04-09 15:24:42 +0200

    remove header form services to intercetor edited

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,15 +1,21 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, Routes } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http'; 
+import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'; // 👈
 import { importProvidersFrom } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HTTP_INTERCEPTORS } from '@angular/common/http'; // 👈
+import { AuthInterceptor } from './interceptor/auth_interceptor';
 import { routes } from './app.routes';
-
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes), 
-    provideHttpClient(), 
+    provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi()), 
     importProvidersFrom(FormsModule),
+    {
+      provide: HTTP_INTERCEPTORS, //
+      useClass: AuthInterceptor,
+      multi: true,
+    },
   ],
 };
